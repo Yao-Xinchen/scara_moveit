ecf988a89b3999a66ddf54afdff62db523d72a90
Yao-Xinchen <yao29@illinois.edu>
2024-02-24 12:43:17 +0800

    EndVel interface added

--- scara_servo/src/scara_servo.cpp
+++ scara_servo/src/scara_servo.cpp
@@ -1,13 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
+#include <arm_interface/msg/detail/end_vel__struct.hpp>
 #include <control_msgs/msg/detail/joint_jog__struct.hpp>
+#include <geometry_msgs/msg/detail/twist_stamped__struct.hpp>
 #include <memory>
 
 #include <moveit_servo/servo_parameters.h>
 #include <moveit_servo/servo.h>
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <rclcpp/subscription.hpp>
+
+#include "arm_interface/msg/end_vel.hpp"
 
 using control_msgs::msg::JointJog;
 using geometry_msgs::msg::TwistStamped;
+using arm_interface::msg::EndVel;
 
 #define EEF_FRAME_ID "link8" // end effector frame
 #define BASE_FRAME_ID "base_link"
@@ -17,30 +23,30 @@ class ScaraServo : public rclcpp::Node
 public:
     ScaraServo() : Node("scara_servo")
     {
-        // joint_pub_ = this->create_publisher<JointJog>("joint_jog", 10);
-        twist_pub_ = this->create_publisher<TwistStamped>("cartesian_velocity", 10);
-
         this->servo_ = init_servo();
         this->servo_->start();
 
-        // joint_msg_ = std::make_unique<JointJog>();
-        twist_msg_ = std::make_unique<TwistStamped>();
-        twist_msg_->header.frame_id = EEF_FRAME_ID;
+        // joint_pub_ = this->create_publisher<JointJog>("joint_jog", 10);
+        twist_pub_ = this->create_publisher<TwistStamped>("cartesian_velocity", 10);
+
+        end_vel_sub_ = this->create_subscription<EndVel>("end_vel", 10, [this](EndVel::SharedPtr msg) {
+            end_vel_callback(msg);
+        });
     }
 
 private:
     // rclcpp::Publisher<JointJog>::SharedPtr joint_pub_;
     rclcpp::Publisher<TwistStamped>::SharedPtr twist_pub_;
-    JointJog::UniquePtr joint_msg_;
-    TwistStamped::UniquePtr twist_msg_;
-    // MY_TODO: Subscription
     std::shared_ptr<moveit_servo::Servo> servo_;
+    
+    rclcpp::Subscription<EndVel>::SharedPtr end_vel_sub_;
+    EndVel::UniquePtr end_vel_msg_;
 
     std::shared_ptr<moveit_servo::Servo> init_servo()
     {
         auto tf_buffer = std::make_shared<tf2_ros::Buffer>(this->get_clock());
         auto planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(
-            this, "robot_description", tf_buffer, "planning_scene_monitor");
+            this->shared_from_this(), "robot_description", tf_buffer, "planning_scene_monitor");
         if (planning_scene_monitor_->getPlanningScene())
         {
             planning_scene_monitor_->startStateMonitor("/joint_states");
@@ -55,10 +61,24 @@ private:
         auto servo_parameters = moveit_servo::ServoParameters::makeServoParameters(this->shared_from_this());
         if (!servo_parameters) RCLCPP_FATAL(this->get_logger(), "Failed to load the servo parameters");
 
-        auto _servo_ = std::make_shared<moveit_servo::Servo>(this->shared_from_this(), planning_scene_monitor_, servo_parameters);
+        auto _servo_ = std::make_shared<moveit_servo::Servo>(this->shared_from_this(), servo_parameters, planning_scene_monitor_);
         if (!_servo_) RCLCPP_FATAL(this->get_logger(), "Failed to initialize the servo");
 
         return _servo_;
+    }
+
+    void end_vel_callback(const EndVel::SharedPtr msg)
+    {
+        TwistStamped::UniquePtr twist_msg = std::make_unique<TwistStamped>();
+        twist_msg->header.stamp = this->now();
+        twist_msg->header.frame_id = EEF_FRAME_ID;
+        twist_msg->twist.linear.x = msg->x;
+        twist_msg->twist.linear.y = msg->y;
+        twist_msg->twist.linear.z = msg->z;
+        twist_msg->twist.angular.x = msg->roll;
+        twist_msg->twist.angular.y = msg->pitch;
+        twist_msg->twist.angular.z = msg->yaw;
+        twist_pub_->publish(std::move(twist_msg));
     }
 };
 
