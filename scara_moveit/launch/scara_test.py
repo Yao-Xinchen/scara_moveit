4deb748ddb722df6ca51be2e92bad110348d1f1b
Yao-Xinchen <yao29@illinois.edu>
2024-02-26 20:29:37 +0800

    add test launch

---
+++ scara_moveit/launch/scara_test.py
@@ -0,0 +1,26 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    moveit_demo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('scara_moveit_config'),
+                        'launch/demo.launch.py')
+        )
+    )
+
+    ld = LaunchDescription([
+        Node(
+            package='scara_motor',
+            executable='scara_motor',
+            name='scara_motor',
+        ),
+    ])
+
+    ld.add_action(moveit_demo_launch)
+
+    return ld
