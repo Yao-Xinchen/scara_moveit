59e79a08755def3b7dadc745296b6a3ddbc1eba1
Yao-Xinchen <yao29@illinois.edu>
2024-05-16 00:34:00 +0800

    zero pos configured

--- scara_hardware/src/scara_hardware.cpp
+++ scara_hardware/src/scara_hardware.cpp
@@ -28,8 +28,8 @@ CallbackReturn ScaraHardware::on_init(const hardware_interface::HardwareInfo & i
     uint count = 7;
     joint_names = {"joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "joint7"};
     motor_names = {"J1", "J2", "J3", "J4", "J5", "J6", "J7"};
-    offsets = {0.0, -0.29, -0.59, 0.0, 0.0, 0.0, 0.0};
-    ratios = {-1070.0, 1.0, 1.0, -20.0, 20.0, 20.0, 108.0};
+    offsets = {0.0, -1.80, 2.1, 1.57*20, 0.0, -1.57*20, 0.0};
+    ratios = {-1070.0, 1.0, 1.0, -20.0, -20.0, -20.0, 108.0};
 #else
     // uint count = node_->declare_parameter("joint_count", 0);
     // joint_names = node_->declare_parameter("joint_names", joint_names);
